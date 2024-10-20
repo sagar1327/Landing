a6d1b174b2490f076fa2958e4e7fa56dfa7c08a9
Sagar Sarkar <sarkars1@my.erau.edu>
2024-10-19 21:25:06 -0400

    Fixes for Gazebo.

--- offboard_py/src/April_Tag_Landing_v2/test_WPpublisher.py
+++ offboard_py/src/April_Tag_Landing_v2/test_WPpublisher.py
@@ -11,16 +11,18 @@ class TestPublisher():
 
         rospy.init_node("test_publisher")
         
-        # self.wamv_coordinate_msg = NavSatFix()
-        # rospy.Subscriber("/wamv/sensors/gps/gps/fix", NavSatFix, callback=self.wamv_coordinate)
+        self.wamv_coordinate_msg = NavSatFix()
+        self.coordinate_received = False
+        rospy.Subscriber("/wamv/sensors/gps/gps/fix", NavSatFix, callback=self.wamv_coordinate)
 
         self.target_wp_msg = NavSatFix()
-        self.target_wp_msg.latitude = 29.182665
-        self.target_wp_msg.longitude = -81.044093
-        self.target_wp_msg.header.frame_id = "map"
+        # # For Testing Only:
+        # self.target_wp_msg.latitude = 29.182665
+        # self.target_wp_msg.longitude = -81.044093
+        # self.target_wp_msg.header.frame_id = "map"
+
         self.target_wp_pub = rospy.Publisher("/minion/kevin/target_wp", NavSatFix, queue_size=3)
 
-        self.coordinate_received = False
 
         self.rate = rospy.Rate(60)
 
@@ -34,11 +36,10 @@ def main():
     TP = TestPublisher()
 
     while not rospy.is_shutdown():
-        # Temporarily commenting for test: 10/18/2024
-        # if TP.coordinate_received:
-        #     TP.target_wp_msg = TP.wamv_coordinate_msg
-        #     # Change the lat and long recieved from boat if required.
-        #     TP.target_wp_msg.longitude = TP.target_wp_msg.longitude - 0.00006
+        if TP.coordinate_received:
+            TP.target_wp_msg = TP.wamv_coordinate_msg
+            # Change the lat and long recieved from boat if required.
+            TP.target_wp_msg.longitude = TP.target_wp_msg.longitude - 0.00006
 
         TP.target_wp_pub.publish(TP.target_wp_msg)
 
